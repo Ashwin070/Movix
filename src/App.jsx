a95ab08e11999162944fa71034a4f56037fb57a8
Ashwin Shaji <ashwinshaji154@gmail.com>
2024-02-05 17:59:09 +0530

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,69 @@
-import { useState,useEffect } from 'react';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import {fetchDataFromApi} from "./utils/api";
-import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration,getGenres } from './store/homeSlice';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import pageNotFound from './pages/404/PageNotFound';
+import pageNotFound from "./pages/404/pageNotFound";
 
 function App() {
-  const dispatch = useDispatch()
-  const {url} = useSelector((state) => state.home)
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   console.log(url);
 
   useEffect(() => {
     fetchApiConfig();
     genresCall();
-  },[])
+  }, []);
 
   const fetchApiConfig = () => {
-    fetchDataFromApi("/configuration")
-      .then((res) => {
-        console.log(res);
-        const url = {
-          backdrop: res.images.secure_base_url + "original",
-          poster: res.images.secure_base_url + "original",
-          profile: res.images.secure_base_url + "original",
-
-        }
-        dispatch(getApiConfiguration(url));
-      })
-  }
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
+  };
 
   const genresCall = async () => {
-    let promises = []
-    let endPoints = ["tv","movie"]
-    let allGenres = {}
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
 
     endPoints.forEach((url) => {
-      promises.push(fetchDataFromApi(`/genre/${url}/list`))
-    })
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
 
     const data = await Promise.all(promises);
-    
-    data.map(({genres}) => {
-      return genres.map((item) => (allGenres[item.id] = item))
-    })
+
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
 
     dispatch(getGenres(allGenres));
-  }
+  };
 
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<pageNotFound />} />
       </Routes>
-    <Footer />
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
